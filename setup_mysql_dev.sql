ef889abbef0203f3595884f66da95bd9fdf9c346
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-10 13:40:25 -0500

    connet the pasword

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- GRANT type_of_permission ON database_name.table_name TO 'username'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- HOW CONNECT THE pasword?;
+-- CONNECT THE pasword;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
