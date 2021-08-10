b92663360a6108c58fa7257800ba7db859dac6f4
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-10 16:10:57 -0500

    test sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,1 +1,10 @@
 -- test script that prepares a MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- GRANT type_of_permission ON database_name.table_name TO 'username'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_dev_pwd';
+-- CONNECT THE pasword;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
