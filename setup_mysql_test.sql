2a9d0c3bb89cbb0aa14d04ed1e25ddefa601fd47
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-10 16:39:34 -0500

    coment line 5

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,8 +2,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- GRANT type_of_permission ON database_name.table_name TO 'username'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_dev_pwd';
+--SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_dev_pwd';
 -- CONNECT THE pasword;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
