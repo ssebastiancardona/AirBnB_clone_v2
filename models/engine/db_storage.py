92c1ceacb7649cc6a0ff5ff003fae7ef2475ff38
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-11 21:23:39 +0000

    Update: correct pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,12 +7,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from os import getenv
 
+
 class DBStorage:
     """Class Storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-         self.__engine = create_engine()
+        self.__engine = create_engine()
 
-    def all(self, cls=None):()
+    def all(self, cls=None): ()
