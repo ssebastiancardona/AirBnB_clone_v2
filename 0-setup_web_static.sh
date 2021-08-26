6dc368aca054838a187ec8a32a290259a68fd524
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-26 17:23:46 -0500

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/current /data/web_static/releases/test/
+chown -R ubuntu:ubuntu /data/
