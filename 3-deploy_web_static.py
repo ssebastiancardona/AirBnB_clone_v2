63e8672f85d559c350822b8b37834b45812ad4f2
ssebastiancardona <ssebastiancardona@gmail.com>
2021-08-26 22:39:54 -0500

    3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# Escriba un script de Fabric (basado en el archivo 2-do_deploy_web_static.py) que cree y distribuya
+# un archivo a sus servidores web, utilizando la función deploy:
